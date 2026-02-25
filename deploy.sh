cf76136547f830d7531ab5c6f6617c89169c3f24
kamiy2743 <kamiy2743@gmail.com>
2026-02-25 17:55:25 +0900

    定期的に疎通確認するまで

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SERVICE_SRC="/home/kamiy2743/workspace/health-check/healthcheck.service"
+SERVICE_DST="/etc/systemd/system/healthcheck.service"
+TIMER_SRC="/home/kamiy2743/workspace/health-check/healthcheck.timer"
+TIMER_DST="/etc/systemd/system/healthcheck.timer"
+
+sudo rsync -av --delete $SERVICE_SRC $SERVICE_DST
+sudo chmod 644 $SERVICE_DST
+sudo rsync -av --delete $TIMER_SRC $TIMER_DST
+sudo chmod 644 $TIMER_DST
+
+sudo systemctl daemon-reload
